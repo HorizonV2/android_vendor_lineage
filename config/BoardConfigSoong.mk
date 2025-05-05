fc6350c3fac9a4495128e7caacd8ef8608a6c166
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-05-06 07:18:37 +0800

    soong: Add support for miui camera mode [2/2]
    Change-Id: I659be528b57b7a6f43107506920974b2d342d1ef
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,9 @@ endif
 ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
     $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
 endif
+ifneq ($(TARGET_USES_MIUI_CAMERA),)
+    $(call soong_config_set,camera,uses_miui_camera,$(TARGET_USES_MIUI_CAMERA))
+endif
 
 define addVar
   SOONG_CONFIG_lineageVarsPlugin += $(1)
